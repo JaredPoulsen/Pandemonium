61764500ebde9487281b54ba234c6a5bc43f7725
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-10-16 17:39:57 -0400

    Update

---
+++ Assets/MVP/cube.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float min = 2f;
+    public float max = 3f;
+    public float speed = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+        min = transform.position.x;
+        max = transform.position.x + 3;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, max - min) + min, transform.position.y, transform.position.z);
+
+    }
+}
