7d6775c2305d521921dfdb5bd8a338f799c9c626
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-10-18 18:25:49 -0400

    Update Enemy

--- Assets/MVP/enemy.cs
+++ Assets/MVP/enemy.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    [SerializeField] 
+    // bullet
+    public float cooldown;
+    private float cooldownTimer;
+    public GameObject bullet;
+    public Transform shootingPos;
+
+    // moving toward player
     private Transform myTransform;
     public Transform target; 
     public int moveSpeed; 
     public int rotationSpeed; 
     public int maxdistance;
+    Renderer rend;
+    //basic stat
+    public int hp = 250;
+    public float timeToWait = 2f;
+    public float done = 0.0f;
+    bool shot = false ;
 
     void Awake()
     {
@@ -18,33 +32,63 @@ public class enemy : MonoBehaviour
 
     void Start()
     {
-       
 
-       
+        rend = GetComponent<Renderer>();
+
     }
 
 
     void Update()
     {
         Debug.DrawLine(target.position, myTransform.position, Color.red);
+        Debug.Log(hp);
 
-
+        // moving toward player
         myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
 
         if (Vector3.Distance(target.position, myTransform.position) <= maxdistance)
         {
-            //$$anonymous$$ove towards target
+            //Move towards target
             myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
 
+
+            // shoot while in distance
+            Shoot();
+
         }
+       
+    }
+    private void Shoot()
+    {
+        transform.LookAt(target);
+
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer > 0)
+        {
+            return;
+        }
+        cooldownTimer = cooldown;
+        GameObject clonebullet = Instantiate(bullet, shootingPos) as GameObject;
+        Destroy(clonebullet, 0.1f);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
-            
+            Destroy(gameObject);     
+        }
+        if (collision.gameObject.tag == "Bullet")
+        {
+            hp -= 10;
+
+            rend.material.SetColor("_Color", Color.red);
+
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
+    
 }
