767b2eab09e68760d492cd90e2e122a6b969e989
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-10-26 16:15:12 -0400

    Enemy Shooting

---
+++ Assets/Script/Enemy/Shooting/EnemyShooting.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    // Randomly Shooting Angle
+
+    public EnemyRayCast detect;
+    public GameObject bullet;
+
+    [Range(0, 10)]
+    public float inaccuracy;
+
+    public float fireRate = 1f;
+    private float nextShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detect.canSeePlayer)
+        {
+            transform.LookAt(detect.playerRef.transform);
+            Shoot();
+        }
+       
+    }
+    void Shoot()
+    {
+        float randomNumberX = Random.Range(-inaccuracy, inaccuracy);
+        float randomNumberY = Random.Range(-inaccuracy, inaccuracy);
+        float randomNumberZ = Random.Range(-inaccuracy, inaccuracy);
+
+        if (Time.time >= nextShot)
+        {
+            GameObject clonebullet = Instantiate(bullet, transform.position + transform.forward * 0.5f + transform.up * 0.25f, transform.rotation) as GameObject;
+            clonebullet.transform.Rotate(randomNumberX, randomNumberY, randomNumberZ);
+            Destroy(clonebullet, 0.05f);
+            nextShot = Time.time + fireRate;
+        }
+    }
+}
