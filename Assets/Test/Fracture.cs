583c5e4c4754cdc53c440fc0b77ba9ad162e959a
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-10-27 15:47:40 -0400

    Fracture by part

---
+++ Assets/Test/Fracture.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fracture : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+        Debug.Log("Hit");
+        rigidbody.isKinematic = false;
+        rigidbody.WakeUp();
+        }
+    }
+}
