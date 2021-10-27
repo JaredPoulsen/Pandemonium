ec00dd2197fcbcc200a8ebf3efb06b9780bb5a32
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-10-26 21:25:51 -0400

    Check Point Update

--- Assets/Julhiecio TPS Controller/Scripts/Scene Management/SceneController.cs
+++ Assets/Julhiecio TPS Controller/Scripts/Scene Management/SceneController.cs
@@ -4,14 +4,21 @@ using UnityEngine.SceneManagement;
 [AddComponentMenu("JU TPS/Scene Management/Scene Controller")]
 public class SceneController : MonoBehaviour
 {
+    /// <summary>
+    /// SceneController inside the Game Management
+    /// Require a empty gameObject with tag "SpawnPoint"
+    /// </summary>
     ThirdPersonController pl;
     public bool ResetLevelWhenPlayerDie;
     public float SecondsToReset = 4;
     public bool ExitGameWhenPressEsc;
     public bool ResetLevelWhenPressP;
+    public GameObject data;
+    //public GameObject cpdata;
     void Start()
-    {
+    {  
         pl = FindObjectOfType<ThirdPersonController>();
+        pl.transform.position = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
     }
     void Update()
     {
@@ -25,11 +32,14 @@ public class SceneController : MonoBehaviour
         }
         if(pl.IsDead == true && IsInvoking("ResetLevel") == false && ResetLevelWhenPlayerDie == true)
         {
+            GameObject cpdata = Instantiate(data, CheckPoint.GetActiveCheckPointPosition(), Quaternion.identity) as GameObject;
+            DontDestroyOnLoad(cpdata);
             Invoke("ResetLevel", SecondsToReset);
         }
     }
     public void ResetLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       
     }
 }
