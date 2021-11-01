a10f66931a49ad3243e2a193e1dabc535ee12365
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-11-01 18:42:57 -0400

    Fix Menu

---
+++ Assets/Game/Main Menu/Scenestuff.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenestuff : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject InstrutionPanel;
+    public GameObject LoadingPanel;
+    public void Start()
+    {
+        InstrutionPanel.gameObject.SetActive(false);
+        LoadingPanel.gameObject.SetActive(false);
+    }
+    public void Play()
+    {
+        MainMenuPanel.gameObject.SetActive(false);
+        InstrutionPanel.gameObject.SetActive(false);
+        LoadingPanel.gameObject.SetActive(true);
+        SceneManager.LoadScene("1");
+    }
+    public void OpenInstruction()
+    {
+        MainMenuPanel.gameObject.SetActive(false);
+        InstrutionPanel.gameObject.SetActive(true);
+    }
+    public void BackToMenu()
+    {
+        InstrutionPanel.gameObject.SetActive(false);
+        MainMenuPanel.gameObject.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
