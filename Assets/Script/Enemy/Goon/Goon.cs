74d0b582780811db53f85aca7168c922890b871d
JRigs-QU22 <jtrigsby@quinnipiac.edu>
2021-11-02 18:49:40 -0400

    Revert "Merge branch 'main' of https://github.com/JRigs-QU22/Pandemonium"
    This reverts commit e669e7ebc84deb502d60cbd45bc38241f04e1ab3, reversing
    changes made to 80af203216eb16e72670672085cc24137f2cba55.

--- Assets/Script/Enemy/Goon/Goon.cs
+++ Assets/Script/Enemy/Goon/Goon.cs
@@ -4,38 +4,16 @@ using UnityEngine;
 
 public class Goon : EnemyBase
 {
-    private float min;
-    private float max;
-
+    
     protected override void Start()
     {
-        base.Start();
-        min = transform.position.z;
-        max = transform.position.z + 15;
+      base.Start();
     }
 
     protected override void Update()
     {
         base.Update();
-        if (canSeePlayer)
-        {
-            transform.LookAt(playerRef.transform);
 
-            if (Vector3.Distance(playerRef.transform.position, gameObject.transform.position) >= 7f)
-            {
-                moveSpeed = 0.01f;
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3(playerRef.transform.position.x, transform.position.y, playerRef.transform.position.z), moveSpeed);
-
-            }
-            else
-            {
-                moveSpeed = 0f;
-            }
-        }
-        else if (!canSeePlayer)
-        {
-            transform.position = new Vector3(transform.position.x,  transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
-        }
     }
 
 }
