19ec2501b59e989b06dbce492aeb9e49d90023b0
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2021-12-14 14:40:52 -0500

    credit page

--- Assets/Game/Main Menu/MainMenu.cs
+++ Assets/Game/Main Menu/MainMenu.cs
@@ -8,27 +8,38 @@ public class MainMenu : MonoBehaviour
     public GameObject MainMenuPanel;
     public GameObject InstrutionPanel;
     public GameObject LoadingPanel;
+    public GameObject CreditPanel;
     public void Start()
     {
         InstrutionPanel.gameObject.SetActive(false);
         LoadingPanel.gameObject.SetActive(false);
+        CreditPanel.gameObject.SetActive(false);
     }
     public void Play()
     {
         MainMenuPanel.gameObject.SetActive(false);
         InstrutionPanel.gameObject.SetActive(false);
+        CreditPanel.gameObject.SetActive(false);
         LoadingPanel.gameObject.SetActive(true);
         SceneManager.LoadScene("GameLevel_v2");
     }
     public void OpenInstruction()
     {
         MainMenuPanel.gameObject.SetActive(false);
+        CreditPanel.gameObject.SetActive(false);
         InstrutionPanel.gameObject.SetActive(true);
     }
     public void BackToMenu()
     {
         InstrutionPanel.gameObject.SetActive(false);
+        CreditPanel.gameObject.SetActive(false);
         MainMenuPanel.gameObject.SetActive(true);
+    }
+    public void Credit()
+    {
+        MainMenuPanel.gameObject.SetActive(false);
+        InstrutionPanel.gameObject.SetActive(false);
+        CreditPanel.gameObject.SetActive(true);
     }
     public void QuitGame()
     {
