748dd583737c0d40ae289d206621d2248df9dff1
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2022-03-08 13:00:22 -0500

    Revert "Pick up after throw"
    This reverts commit 1609cacedfe37b4376d9ab6ec9a14e1fd260f06b.

--- Assets/Script/Throwing Prefab/DestroyThrowing.cs
+++ Assets/Script/Throwing Prefab/DestroyThrowing.cs
@@ -4,25 +4,15 @@ using UnityEngine;
 
 public class DestroyThrowing : MonoBehaviour
 {
-    private Collider collider;
-    public GameObject children;
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<Collider>();
-        children.gameObject.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Invoke(nameof(changeCollider), 5);
-        Destroy(gameObject, 20f);
-    }
-    void changeCollider()
-    {
-        children.gameObject.SetActive(true);
-        Debug.Log("changed");
-        collider = GetComponentInChildren<Collider>();
+        Destroy(gameObject, 3f);
     }
 }
