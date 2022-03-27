026dc253409e7fb7841df588f249c537c0478449
Son Nguyen <56080098+sonn4820@users.noreply.github.com>
2022-03-27 19:56:48 -0400

    fix

--- Assets/Script/Player/Laser.cs
+++ Assets/Script/Player/Laser.cs
@@ -8,26 +8,31 @@ public class Laser : MonoBehaviour
 {
     private LineRenderer lr;
     public GameObject LaserPosition;
+
+    public LayerMask CrosshairHitMask;
+    public CamPivotController MyPivotCamera;
+    public Transform MyCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        MyPivotCamera = FindObjectOfType<CamPivotController>();
+        MyCamera = MyPivotCamera.mCamera.transform;
+        CrosshairHitMask = FindObjectOfType<ThirdPersonController>().CrosshairHitMask;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(LaserPosition.transform.position, transform.forward, out hit))
+        RaycastHit CrosshairHit;
+
+        if (Physics.Raycast(MyCamera.transform.position + MyCamera.transform.forward * MyPivotCamera.Distance / 2, MyCamera.transform.forward, out CrosshairHit, 500, CrosshairHitMask))
         {
-            if (hit.collider)
-            {
-                lr.SetPosition(1, new Vector3(0,0,hit.distance));
-            }else
-            {
-                lr.SetPosition(1, new Vector3(0, 0, 5000));
-            }
+            this.transform.LookAt(CrosshairHit.point);
+            lr.SetPosition(1, new Vector3(0, 0, CrosshairHit.distance));
         }
+          
 
     }
 }
