8b5ee824acbd6fbfce1b0a3937673020ea76aef9
JaredPoulsen <jcpoulsen@quinnipiac.edu>
2022-04-13 11:44:26 -0400

    New Health Bar
    Created new health bar UI and added throw animation to animations folder under player folder.

--- Assets/MVP/hpBar.cs
+++ Assets/MVP/hpBar.cs
@@ -6,13 +6,28 @@ using UnityEngine.UI;
 public class hpBar : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Slider slider;
+    //public Slider slider;
     public ThirdPersonController player;
+    public float lerpSpeed;
+    public Image healthBar;
+    public float maxHealth = 100f;
     // Update is called once per frame
+
     void Update()
     {
-        slider.maxValue = 250;
-        slider.value = player.Health;
-        
+        lerpSpeed = 3f * Time.deltaTime;
+        HealthBarFiller();
+        ColorChanger();
+    }
+
+    void HealthBarFiller()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (player.Health / 100), lerpSpeed);
+    }
+
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(Color.red, Color.green, (player.Health / 100));
+        healthBar.color = healthColor;
     }
 }
